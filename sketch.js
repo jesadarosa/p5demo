8a5580b048bfc1c78bc4c81cb37c5fc7e276e6f9
jesadarosa <131052987+jesadarosa@users.noreply.github.com>
2024-04-20 12:55:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,10 +11,10 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1180, 760);
+  createCanvas(windowWidth, windowHeight);
   
   // Resize images
-  backgroundImg.resize(1180, 760);
+  backgroundImg.resize(windowWidth, windowHeight);
   catcherImg.resize(150, 0);
   fallingObjectImg.resize(45, 0);
   
