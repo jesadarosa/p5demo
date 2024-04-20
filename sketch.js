875731fd771ecc05533cd619b5ff19d7f8726bb9
jesadarosa <131052987+jesadarosa@users.noreply.github.com>
2024-04-20 13:27:38 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,16 +15,21 @@ function setup() {
   
   // Resize images
   backgroundImg.resize(windowWidth, windowHeight);
-  catcherImg.resize(1000, 0);
-  fallingObjectImg.resize(1000, 0);
+  catcherImg.resize(100, 0); // Adjust the width of the catcher image
+  fallingObjectImg.resize(50, 0); // Adjust the width of the falling object image
   
+  //Calculate initial positions
+  let catcherX = windowWidth / 2; // Center the catcher horizontally
+  let catcherY = windowHeight - catcherImg.height; // Place the catcher at the bottom of the window
+  let fallingObjectX = random(windowWidth); // Randomize the falling object's initial X position
+  let fallingObjectY = 0; // Start the falling object at the top of the window
+
   //Create catcher 
-   catcher = new Sprite(catcherImg, 200 , 1028, "k");
+  catcher = new Sprite(catcherImg, catcherX, catcherY, "k");
 
   //Create falling object
-  fallingObject = new Sprite(fallingObjectImg, 100, 0);
+  fallingObject = new Sprite(fallingObjectImg, fallingObjectX, fallingObjectY);
   fallingObject.velocity.y = 2;
-  
 }
 
 function draw() {
@@ -42,32 +47,28 @@ function draw() {
   if (fallingObject.y >= height) {
     fallingObject.y = 0;
     fallingObject.x = random(width);
-    fallingObject.vel.y = random(1, 5);
+    fallingObject.velocity.y = random(1, 5);
 
     // Spicy 
     score -= 1;
   }
   // Move catcher
-  if (kb.pressing("left")) {
-    catcher.vel.x = -3;
-  } else if (kb.pressing("right")) {
-    catcher.vel.x = 3;
+  if (keyIsDown(LEFT_ARROW)) {
+    catcher.velocity.x = -3;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    catcher.velocity.x = 3;
   } else {
-    catcher.vel.x = 0;
+    catcher.velocity.x = 0;
   }
 
   // Stop catcher at edges of screen
-  if (catcher.x < 50) {
-    catcher.x = 50;
-  } else if (catcher.x > 350) {
-    catcher.x = 350;
-  }
+  catcher.position.x = constrain(catcher.position.x, 0, width - catcher.width);
 
   // If fallingObject collides with catcher, move back to random position at top
   if (fallingObject.collides(catcher)) {
     fallingObject.y = 0;
     fallingObject.x = random(width);
-    fallingObject.vel.y = random(1, 5);
+    fallingObject.velocity.y = random(1, 5);
     fallingObject.direction = "down";
     score += 1;
   }
@@ -80,22 +81,22 @@ function draw() {
 
   // Medium Losing screen
   if (score < 0){
-    
-  background(224, 224, 224);
+    background(224, 224, 224);
     // Draw sprites off of screen
-  catcher.pos = { x: 600, y: -300 };
-  fallingObject.pos = { x: -100, y: 0 };
+    catcher.position.x = 600;
+    catcher.position.y = -300;
+    fallingObject.position.x = -100;
+    fallingObject.position.y = 0;
 
-  noStroke();
-  fill(0);
-  text("You gave in", width / 2 - 50, height / 2 - 30);
-  textSize(12);
-  text("Click the mouse anywhere to play again.", width / 2 - 120, height / 2);
+    noStroke();
+    fill(0);
+    text("You gave in", width / 2 - 50, height / 2 - 30);
+    textSize(12);
+    text("Click the mouse anywhere to play again.", width / 2 - 120, height / 2);
 
     if (mouseIsPressed) {
       restart();
     }
-    
   }
 
   // Spicy - Check to see if player won
@@ -117,8 +118,10 @@ function youWin() {
   background(224, 224, 224);
 
   // Draw sprites off of screen
-  catcher.pos = { x: 600, y: -300 };
-  fallingObject.pos = { x: -100, y: 0 };
+  catcher.position.x = 600;
+  catcher.position.y = -300;
+  fallingObject.position.x = -100;
+  fallingObject.position.y = 0;
 
   // Draw end of game text
   noStroke();
@@ -135,7 +138,8 @@ function restart() {
   score = 0;
 
   // Reset sprites
-  catcher.pos = { x: 200, y: 380 };
+  catcher.position.x = 200;
+  catcher.position.y = 380;
   fallingObject.y = 0;
   fallingObject.x = random(width);
   fallingObject.velocity.y = random(1, 5);
